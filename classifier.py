33b7e9d1b57de1262bf5235be9cc7e6c6c1b375f
paulo-chagas <paulo.robertojr100@gmail.com>
2017-10-18 17:10:54 -0300

    adding files

---
+++ classifier.py
@@ -0,0 +1,141 @@
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input
+from keras.callbacks import TensorBoard
+
+import time
+import os, errno
+
+# Create path for TensorBoard logs
+path = "./logs_tb/tb_{}".format(time.time())
+print path
+
+# Create folder if does not exist
+try:
+    os.makedirs(path)
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+
+tensorboard = TensorBoard(log_dir=path,
+                        histogram_freq=0, 
+                        write_graph=True, 
+                        write_images=True)
+
+# dimensions of our images.
+img_width, img_height = 224, 224
+
+train_data_dir = './data_dir' #contains two classes cats and dogs
+validation_data_dir = './validation' #contains two classes cats and dogs
+
+nb_train_samples = 314
+nb_validation_samples = 40
+nb_epoch = 10
+
+# create the base pre-trained model
+print "\n\n\n\n"
+print "Creating model InceptionV3 --  no weights"
+base_model = InceptionV3(weights=None, include_top=False)
+weights_imagenet = './inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+print "Loading weights..."
+base_model.load_weights(weights_imagenet)
+print "Done"
+
+# add a global spatial average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+# and a logistic layer -- let's say we have 200 classes
+predictions = Dense(4, activation='softmax')(x)
+
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+# compile the model (should be done *after* setting layers to non-trainable)
+#model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# prepare data augmentation configuration
+train_datagen = ImageDataGenerator()
+ #       rescale=1./255)#,
+ #       shear_range=0.2,
+ #       zoom_range=0.2,
+ #       horizontal_flip=True)
+
+test_datagen = ImageDataGenerator()#rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=16,
+    class_mode='categorical'
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=16,
+    class_mode='categorical'
+)
+
+print "Start history model - training the top layers"
+history = model.fit_generator(
+    train_generator,
+    epochs=nb_epoch,
+    steps_per_epoch=(nb_train_samples/16),
+    validation_data=validation_generator,
+    validation_steps=(nb_validation_samples/16)) #1020
+
+# at this point, the top layers are well trained and we can start fine-tuning
+# convolutional layers from inception V3. We will freeze the bottom N layers
+# and train the remaining top layers.
+
+# let's visualize layer names and layer indices to see how many layers
+# we should freeze:
+print "Layers"
+for i, layer in enumerate(base_model.layers):
+   print(i, layer.name)
+
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 172 layers and unfreeze the rest:
+for layer in model.layers[:172]:
+   layer.trainable = False
+for layer in model.layers[172:]:
+   layer.trainable = True
+
+# we need to recompile the model for these modifications to take effect
+# we use SGD with a low learning rate
+from keras.optimizers import SGD
+model.compile(optimizer=SGD(lr=0.0001, decay=1e-6, momentum=0.9), 
+        loss='categorical_crossentropy', 
+        metrics=['accuracy'])
+
+# we train our model again (this time fine-tuning the top 2 inception blocks
+# alongside the top Dense layers
+#model.fit_generator(...)
+# fine-tune the model
+
+print "Fine-Tuning the model..."
+model.fit_generator(
+        train_generator,
+        epochs=nb_epoch,
+        steps_per_epoch=(nb_train_samples/16),
+        validation_data=validation_generator,
+        validation_steps=(nb_validation_samples/16),
+        callbacks=[tensorboard])
+
+print "Done"
+print "Saving model"
+model.save("inception_retrained_model2.h5")
+print "Model saved"
---
+++ load_classifier.py
@@ -0,0 +1,52 @@
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.inception_v3 import preprocess_input, decode_predictions
+import numpy as np
+
+img_width, img_height = 224, 224
+
+labels = ['bar', 'bubble', 'donut', 'line']
+
+print "Loading model"
+model = load_model('inception_retrained_model1')
+model.load_weights('inception_retrained_model1')
+print "Model loaded"
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+print "Compile done" 
+
+# predicting images
+img0 = image.load_img('000.jpg', target_size=(img_width, img_height))
+x0 = image.img_to_array(img0)
+x0 = np.expand_dims(x0, axis=0)
+x0 = preprocess_input(x0)
+print x0.shape
+
+img1 = image.load_img('001.jpg', target_size=(img_width, img_height))
+x1 = image.img_to_array(img1)
+x1 = np.expand_dims(x1, axis=0)
+x1 = preprocess_input(x1)
+print x1.shape
+
+img2 = image.load_img('002.jpg', target_size=(img_width, img_height))
+x2 = image.img_to_array(img2)
+x2 = np.expand_dims(x2, axis=0)
+x2 = preprocess_input(x2)
+print x2.shape
+
+img3 = image.load_img('003.jpg', target_size=(img_width, img_height))
+x3 = image.img_to_array(img3)
+x3 = np.expand_dims(x3, axis=0)
+x3 = preprocess_input(x3)
+print x3.shape
+
+#images = np.vstack([x0, x1, x2, x3])
+print "Prediction"
+for i in [x0, x1, x2, x3]:
+	preds = model.predict(i)
+	print preds
+	classes = np.argmax(preds)
+	print labels[classes]
+	#print('Predicted:', decode_predictions(preds, top=3)[0])
+	print "------------"
