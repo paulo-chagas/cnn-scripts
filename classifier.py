ea786295d3e8ddaa27dd634d5036b12a00722467
paulo-chagas <paulo.robertojr100@gmail.com>
2017-10-20 16:51:01 -0300

    update classifier and plots

--- classifier.py
+++ classifier.py
@@ -1,3 +1,10 @@
+# encoding=utf8  
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+print "Acurácia"
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
 from keras.models import Model
@@ -29,14 +36,14 @@ tensorboard = TensorBoard(log_dir=path,
                         write_images=True)
 
 # dimensions of our images.
-img_width, img_height = 224, 224
+img_width, img_height = 299, 299
 
 train_data_dir = '../data_dir' #contains two classes cats and dogs
 validation_data_dir = '../validation' #contains two classes cats and dogs
 
 nb_train_samples = 314
 nb_validation_samples = 40
-nb_epoch = 1000
+nb_epoch = 50
 
 # create the base pre-trained model
 print "\n\n\n\n"
@@ -68,25 +75,25 @@ for layer in base_model.layers:
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # prepare data augmentation configuration
-train_datagen = ImageDataGenerator()
- #       rescale=1./255)#,
+train_datagen = ImageDataGenerator(
+         rescale=1./255)#,
  #       shear_range=0.2,
  #       zoom_range=0.2,
  #       horizontal_flip=True)
 
-test_datagen = ImageDataGenerator()#rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
-    batch_size=32,
+    batch_size=16,
     class_mode='categorical'
 )
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_width, img_height),
-    batch_size=32,
+    batch_size=16,
     class_mode='categorical'
 )
 
@@ -94,9 +101,35 @@ print "Start history model - training the top layers"
 history = model.fit_generator(
     train_generator,
     epochs=nb_epoch,
-    steps_per_epoch=(nb_train_samples/32),
+    steps_per_epoch=(nb_train_samples/16),
     validation_data=validation_generator,
-    validation_steps=(nb_validation_samples/32)) #1020
+    validation_steps=(nb_validation_samples/16)) #1020
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Acurácia - Treino Top Layers')
+plt.ylabel('Acurácia')
+plt.xlabel('Época')
+plt.legend(['Treino', 'Teste'], loc='upper left')
+
+plt.savefig('../imgs/result_acc_toplayers_1.png', bbox_inches='tight')
+
+plt.clf()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss - Treino Top Layers')
+plt.ylabel('Loss')
+plt.xlabel('Época')
+plt.legend(['Treino', 'Teste'], loc='upper left')
+
+plt.savefig('../imgs/result_loss_toplayers_1.png', bbox_inches='tight')
+
+
 
 # at this point, the top layers are well trained and we can start fine-tuning
 # convolutional layers from inception V3. We will freeze the bottom N layers
@@ -118,7 +151,7 @@ for layer in model.layers[172:]:
 # we need to recompile the model for these modifications to take effect
 # we use SGD with a low learning rate
 from keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.001, decay=1e-6, momentum=0.9), 
+model.compile(optimizer=SGD(lr=0.0001, decay=1e-6, momentum=0.9), 
         loss='categorical_crossentropy', 
         metrics=['accuracy'])
 
@@ -138,18 +171,30 @@ history = model.fit_generator(
 
 print "Done"
 print "Saving model"
-model.save("inception_retrained_model{}.h5".format(time.time()))
+model.save("../models/inception_retrained_model_1")
 print "Model saved"
 
+plt.clf()
 # list all data in history
 print(history.history.keys())
 # summarize history for accuracy
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+plt.title('Acurácia - Fine-Tuning')
+plt.ylabel('Acurácia')
+plt.xlabel('Época')
+plt.legend(['Treino', 'Teste'], loc='upper left')
 
-plt.savefig('../result_new.png', bbox_inches='tight')
-plt.show()
+plt.savefig('../imgs/result_acc_finetuning_1.png', bbox_inches='tight')
+
+plt.clf()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss - Fine-Tuning')
+plt.ylabel('Loss')
+plt.xlabel('Época')
+plt.legend(['Treino', 'Teste'], loc='upper left')
+
+plt.savefig('../imgs/result_loss_finetuning_1.png', bbox_inches='tight')
